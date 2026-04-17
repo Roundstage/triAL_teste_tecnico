3aadad20987460ea601ecb6f75b621cded1bd692
Allison Martins <allison.roundstage@gmail.com>
2026-04-17 04:35:56 -0300

    Fix: Swagger não estava nos providers.

--- apps/backend/bootstrap/providers.php
+++ apps/backend/bootstrap/providers.php
@@ -6,4 +6,5 @@ return [
     AppServiceProvider::class,
     Illuminate\View\ViewServiceProvider::class,
     Illuminate\Translation\TranslationServiceProvider::class,
+    L5Swagger\L5SwaggerServiceProvider::class,
 ];
